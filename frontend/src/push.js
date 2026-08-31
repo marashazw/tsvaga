90374bb119c3214450f84565aad540234f4f5ae5
marashazw <pmarasha@gmail.com>
2026-08-31 05:47:37 +0200

    update push

--- frontend/src/push.js
+++ frontend/src/push.js
@@ -32,10 +32,10 @@ export async function enablePushNotifications() {
     });
 
     // 📍 TEMPORARY LOG FOR GRABBING NATIVE TOKENS:
-    console.log("MOBILE_SUBSCRIPTION_TOKEN_JSON:", JSON.stringify(subscription.toJSON()));
+    //console.log("MOBILE_SUBSCRIPTION_TOKEN_JSON:", JSON.stringify(subscription.toJSON()));
 
     // 📍 TEMPORARY POPUP TO UNMASK THE TOKEN ON YOUR PHONE SCREEN:
-    alert("MOBILE_TOKEN: " + JSON.stringify(subscription.toJSON()));
+    //alert("MOBILE_TOKEN: " + JSON.stringify(subscription.toJSON()));
 
     await api.post('/users/me/push-subscription', subscription.toJSON());
     return 'granted';
